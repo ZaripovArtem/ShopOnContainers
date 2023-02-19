b046055c735dc13aba6d418fb56456c2de3a855b
Артем <artemzaripov2002@gmail.com>
2023-02-19 19:34:17 +0300

    Исправление табуляции

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -10,162 +10,162 @@ namespace Catalog.API.Controllers;
 [ApiController]
 public class CatalogController : ControllerBase
 {
-	private readonly CatalogContext db;
-	public CatalogController(CatalogContext catalogContext)
+    private readonly CatalogContext db;
+    public CatalogController(CatalogContext catalogContext)
 	{
 		db = catalogContext;
 	}
 
-	// GET api/[controller]/items[?pageSize=&pageIndex=]
-	[HttpGet]
-	[Route("items")]
-	public async Task<ActionResult<PaginatedItemsViewModel<CatalogItem>>> GetItemsAsync([FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
+    // GET api/[controller]/items[?pageSize=&pageIndex=]
+    [HttpGet]
+    [Route("items")]
+    public async Task<ActionResult<PaginatedItemsViewModel<CatalogItem>>> GetItemsAsync([FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
 	{
-		var totalItems = await db.CatalogItems
-			.CountAsync();
+        var totalItems = await db.CatalogItems
+            .CountAsync();
 
-		var itemsOnCurrentPage = await db.CatalogItems
-			.OrderBy(x => x.Id)
-			.Skip(pageSize*pageIndex)
-			.Take(pageSize)
-			.ToListAsync();
+        var itemsOnCurrentPage = await db.CatalogItems
+            .OrderBy(x => x.Id)
+            .Skip(pageSize*pageIndex)
+            .Take(pageSize)
+            .ToListAsync();
 
         var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnCurrentPage);
-		return Ok(model);
+        return Ok(model);
 	}
 
     // GET api/[controller]/items/id
     [HttpGet]
-	[Route("items/{id:int}")]
-	public async Task<ActionResult<CatalogItem>> GetItemByIdAsync(int id)
+    [Route("items/{id:int}")]
+    public async Task<ActionResult<CatalogItem>> GetItemByIdAsync(int id)
 	{
-		if (id < 0)
-		{
-			return BadRequest();
-		}
+        if (id < 0)
+        {
+            return BadRequest();
+        }
 
-		var item = await db.CatalogItems.SingleOrDefaultAsync(item => item.Id == id);
+        var item = await db.CatalogItems.SingleOrDefaultAsync(item => item.Id == id);
 
-		if(item != null)
-		{
-			return item;
-		}
+        if (item != null)
+        {
+            return item;
+        }
 
-		return NotFound();
-	}
+        return NotFound();
+    }
 
     // GET api/[controller]/items/withname/name[?pageSize=&pageIndex=]
     [HttpGet]
     [Route("items/withname/{name:minlength(1)}")]
-	public async Task<ActionResult<PaginatedItemsViewModel<CatalogItem>>> ItemsWithNameAsync(string name, [FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
+    public async Task<ActionResult<PaginatedItemsViewModel<CatalogItem>>> ItemsWithNameAsync(string name, [FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
 	{
         var totalItems = await db.CatalogItems
-			.Where(item => item.Name.Contains(name))
+            .Where(item => item.Name.Contains(name))
             .CountAsync();
 
-		var itemsOnCurrentPage = await db.CatalogItems
-			.Where(item => item.Name.Contains(name))
-			.Skip(pageSize*pageIndex)
-			.Take(pageSize)
-			.ToListAsync();
+        var itemsOnCurrentPage = await db.CatalogItems
+            .Where(item => item.Name.Contains(name))
+            .Skip(pageSize*pageIndex)
+            .Take(pageSize)
+            .ToListAsync();
 
-		return new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnCurrentPage);
+        return new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnCurrentPage);
 	}
 
-	// POST api/[controller]/items
-	[HttpPost]
-	[Route("items")]
-	public async Task<ActionResult> CreateItemAsync([FromBody] CatalogItem catalogItem)
-	{
-		var item = new CatalogItem
-		{
-			Name = catalogItem.Name,
-			Description = catalogItem.Description,
-			Price = catalogItem.Price,
-			CatalogBrandId = catalogItem.CatalogBrandId,
-			CatalogTypeId = catalogItem.CatalogTypeId
-		};
+    // POST api/[controller]/items
+    [HttpPost]
+    [Route("items")]
+    public async Task<ActionResult> CreateItemAsync([FromBody] CatalogItem catalogItem)
+    {
+        var item = new CatalogItem
+        {
+            Name = catalogItem.Name,
+            Description = catalogItem.Description,
+            Price = catalogItem.Price,
+            CatalogBrandId = catalogItem.CatalogBrandId,
+            CatalogTypeId = catalogItem.CatalogTypeId
+        };
 
-		db.CatalogItems.Add(item);
-		await db.SaveChangesAsync();
-		return Ok();
-	}
+        db.CatalogItems.Add(item);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
 
     // PUT api/[controller]/items
     [HttpPut]
     [Route("items")]
-	public async Task<ActionResult> UpdateItemAsync([FromBody] CatalogItem catalogItem)
-	{
-		db.CatalogItems.Update(catalogItem);
-		await db.SaveChangesAsync();
-		return Ok(); 
-	}
+    public async Task<ActionResult> UpdateItemAsync([FromBody] CatalogItem catalogItem)
+    {
+        db.CatalogItems.Update(catalogItem);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
 
     // DELETE api/[controller]/items
     [HttpDelete]
-	[Route("items/{id:int}")]
-	public async Task<ActionResult> DeleteItemAsync(int id)
-	{
-		var item = await db.CatalogItems.FindAsync(id);
-		
-		if(item == null)
-		{
-			return NotFound();
-		}
+    [Route("items/{id:int}")]
+    public async Task<ActionResult> DeleteItemAsync(int id)
+    {
+        var item = await db.CatalogItems.FindAsync(id);
 
-		db.CatalogItems.Remove(item);
-		await db.SaveChangesAsync();
-		return Ok();
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        db.CatalogItems.Remove(item);
+        await db.SaveChangesAsync();
+        return Ok();
 	}
 
     // GET api/[controller]/brands
     [HttpGet]
-	[Route("brands")]
-	public async Task<ActionResult<List<CatalogBrand>>> GetBrandsAsync()
-	{
-		return await db.CatalogBrands.ToListAsync();
-	}
-
-	// POST api/[controller]/brands
-	[HttpPost]
     [Route("brands")]
-	public async Task<ActionResult> CreateBrandAsync([FromBody] CatalogBrand catalogBrand)
-	{
-		var brand = new CatalogBrand
-		{
-			Brand = catalogBrand.Brand
-		};
-
-        db.CatalogBrands.Add(brand);
-		await db.SaveChangesAsync();
-		return Ok();
+    public async Task<ActionResult<List<CatalogBrand>>> GetBrandsAsync()
+    {
+        return await db.CatalogBrands.ToListAsync();
     }
 
-	// PUT api/[controller]/brands
-	[HttpPut]
+    // POST api/[controller]/brands
+    [HttpPost]
     [Route("brands")]
-	public async Task<ActionResult> UpdateBrandAsync([FromBody] CatalogBrand catalogBrand)
-	{
-		db.CatalogBrands.Update(catalogBrand);
-		await db.SaveChangesAsync();
-		return Ok();
-	}
+    public async Task<ActionResult> CreateBrandAsync([FromBody] CatalogBrand catalogBrand)
+    {
+        var brand = new CatalogBrand
+        {
+            Brand = catalogBrand.Brand
+        };
 
-	// DELETE api/[controller]/brands
-	[HttpDelete]
+        db.CatalogBrands.Add(brand);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
+
+    // PUT api/[controller]/brands
+    [HttpPut]
+    [Route("brands")]
+    public async Task<ActionResult> UpdateBrandAsync([FromBody] CatalogBrand catalogBrand)
+    {
+        db.CatalogBrands.Update(catalogBrand);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
+
+    // DELETE api/[controller]/brands
+    [HttpDelete]
     [Route("brands/{id:int}")]
-	public async Task<ActionResult> DeleteBrandAsync(int id)
-	{
+    public async Task<ActionResult> DeleteBrandAsync(int id)
+    {
         var brand = await db.CatalogBrands.FindAsync(id);
 
-		if(brand == null)
-		{
-			return NotFound();
-		}
+        if (brand == null)
+        {
+            return NotFound();
+        }
 
-		db.CatalogBrands.Remove(brand);
-		await db.SaveChangesAsync();
-		return Ok();
+        db.CatalogBrands.Remove(brand);
+        await db.SaveChangesAsync();
+        return Ok();
     }
 
     // GET api/[controller]/types
